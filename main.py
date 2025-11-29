5657eef29653e8bfc3e1b6b7bf97c316bc4786d1
Amircphys <amirchik052012@gmail.com>
2025-11-29 18:37:04 +0300

    move items file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-from items_views import router as items_router
+from items.items_views import router as items_router
 from users.views import router as users_router
 
 app = FastAPI()
 app.include_router(items_router, tags=["Items"])
 app.include_router(users_router)
+
 
 @app.get("/")
 def hello_index():
